91010a2c8035b8a598ef0b0445f1d3baa6612471
Mats Andreassen <matsand@live.no>
2021-02-04 11:59:52 +0100

    Oppdatert VTFK-temaet med litt roligere farger

--- scripts/storybook/manager.js
+++ scripts/storybook/manager.js
@@ -2,6 +2,8 @@ import { addons } from '@storybook/addons'
 // import { themes } from '@storybook/theming'
 import vtfkTheme from './vtfkTheme'
 
+import './storybook.css'
+
 addons.setConfig({
-	theme: vtfkTheme,
+  theme: vtfkTheme
 })
--- scripts/storybook/vtfkTheme.js
+++ scripts/storybook/vtfkTheme.js
@@ -1,38 +1,37 @@
 import { create } from '@storybook/theming/create'
 
 export default create({
-	base: 'light',
+  base: 'light',
 
-	colorPrimary: '#E3081B',
-	colorSecondary: '#E3081B',
+  colorPrimary: '#BEDACA',
+  colorSecondary: '#BEDACA',
 
-	// UI
-	appBg: '#EFEFEF',
-	appContentBg: '#fff',
-	appBorderColor: 'transparent',
-	appBorderRadius: 0,
+  // UI
+  appBg: '#EFEFEF',
+  appContentBg: '#fff',
+  appBorderColor: 'transparent',
+  appBorderRadius: 4,
 
-	// Typography
-	fontBase:
-	'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-	fontCode: 'monospace',
+  // Typography
+  fontBase: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  fontCode: 'monospace',
 
-	// Text colors
-	textColor: '#000',
-	textInverseColor: 'rgba(255,255,255,0.9)',
+  // Text colors
+  textColor: '#000',
+  textInverseColor: '#000',
 
-	// Toolbar default and active colors
-	barTextColor: '#000',
-	barSelectedColor: '#000',
-	barBg: '#FED603',
+  // Toolbar default and active colors
+  barTextColor: '#005260',
+  barSelectedColor: '#005260',
+  barBg: '#BEDACA',
 
-	// Form colors
-	inputBg: 'white',
-	inputBorder: 'transparent',
-	inputTextColor: '#000',
-	inputBorderRadius: 4,
+  // Form colors
+  inputBg: '#EFEFEF',
+  inputBorder: 'transparent',
+  inputTextColor: '#000',
+  inputBorderRadius: 4,
 
-	brandTitle: 'VTFK komponentbibliotek',
-	brandUrl: 'https://example.com',
-	brandImage: 'https://designmanual.vtfk.no/css/images/VT-logo.svg',
+  brandTitle: 'Komponentbibliotek VTFK',
+  brandUrl: 'https://komponenter.vtfk.no',
+  brandImage: 'https://designmanual.vtfk.no/css/images/VT-logo.svg'
 })
