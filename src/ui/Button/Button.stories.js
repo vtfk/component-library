b02fe6608fef1ca548341c5535eb02e489054457
Mats Andreassen <matsand@live.no>
2021-02-09 00:35:04 +0100

    Oppdatert story for knapp

--- src/ui/Button/Button.stories.js
+++ src/ui/Button/Button.stories.js
@@ -1,27 +1,71 @@
 import React from 'react'
+import { withKnobs, select, text, boolean } from '@storybook/addon-knobs'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
+import { Button, IconButton } from '.'
 
-import { Button, IconButtonLink } from '.'
+export default getConfig({
+  title: 'Button',
+  component: Button,
+  decorators: [withKnobs]
+})
 
-export default getConfig(
-  { title: 'Button', component: Button }
-)
+const types = {
+  Primary: 'primary',
+  Secondary: 'secondary',
+  Secondary2: 'secondary2'
+}
+
+const icons = ['home', 'arrowRight', 'arrowLeft', 'add', 'check', 'close', 'search']
 
 export function Basic () {
   return (
-    <Button type='a'>Dette er knappeteksten</Button>
+    <Button
+      type={select('Button type', types, 'primary')}
+      spinner={boolean('Spinning', false)}
+      disabled={boolean('Disabled', false)}
+    >
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
+  )
+}
+
+export function Primary () {
+  return (
+    <Button type='primary'>
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
+  )
+}
+
+export function Secondary () {
+  return (
+    <Button type='secondary'>
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
+  )
+}
+
+export function Secondary2 () {
+  return (
+    <Button type='secondary2'>
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
   )
 }
 
 export function Disabled () {
   return (
-    <Button icon='home' disabled>Dette er knappeteksten</Button>
+    <Button disabled>
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
   )
 }
 
 export function ShowSpinner () {
   return (
-    <Button icon='home' spinner>Dette er knappeteksten</Button>
+    <Button spinner>
+      {text('Button text', 'Dette er knappeteksten')}
+    </Button>
   )
 }
 
