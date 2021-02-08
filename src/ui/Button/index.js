8aa1d79007722f5ceb2e4457e2e32846b7a9f531
Mats Andreassen <matsand@live.no>
2021-02-09 00:36:01 +0100

    Oppdatert komponent til å videresende referansen til knappen, samt å definere standardverdi på feltene så story blir korrekt dokumentert

--- src/ui/Button/index.js
+++ src/ui/Button/index.js
@@ -1,49 +1,56 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 
-import { Link } from '../Typography'
 import { Icon } from '../Icon'
 import { Spinner } from '../Spinner'
 
 import './styles.scss'
 
-export function Button ({ className, type, spinner, disabled, children, ...props }) {
-  return (
-    <button
-      className={`button button-${type || 'primary'} ${className}`}
-      disabled={disabled || spinner || false}
-      {...props}
-    >
-      {spinner ? <Spinner /> : children}
-    </button>
-  )
-}
-
-export const IconButtonLink = forwardRef(({ className, type, icon, children, ...props }, ref) => (
+export const Button = forwardRef(({ className, type, spinner, disabled, children, ...props }, ref) => (
   <button
-    className={`icon-button-link ${type ? `icon-button-link-${type}` : ''} ${className}`}
-      {...props}
-    >
-      <div className='icon-button-link-icon'>
-        <Icon name={icon} size='small' />
-      </div>
-      <div className='icon-button-link-text'>
-        {children}
-      </div>
+    className={`button button-${type || 'primary'} ${className}`}
+    disabled={disabled || spinner || false}
+    ref={ref}
+    {...props}
+  >
+    {spinner ? <Spinner /> : children}
   </button>
 ))
 
+export const IconButton = forwardRef(({ className, type, icon, children, ...props }, ref) => (
+  <button
+    className={`icon-button-link ${type ? `icon-button-link-${type}` : ''} ${className}`}
+    ref={ref}
+    {...props}
+  >
+    <div className='icon-button-link-icon'>
+      <Icon name={icon || 'add'} size='small' />
+    </div>
+    <div className='icon-button-link-text'>
+      {children}
+    </div>
+  </button>
+))
+
+export const IconButtonLink = IconButton
+
 Button.propTypes = {
+  children: PropTypes.string.isRequired,
   className: PropTypes.string,
-  type: PropTypes.string,
   disabled: PropTypes.bool,
   spinner: PropTypes.bool,
-  children: PropTypes.string.isRequired
+  type: PropTypes.oneOf(['primary', 'secondary', 'secondary2'])
 }
 
-IconButtonLink.propTypes = {
+Button.defaultProps = {
+  disabled: false,
+  spinner: false,
+  type: 'primary'
+}
+
+IconButton.propTypes = {
+  children: PropTypes.string.isRequired,
   className: PropTypes.string,
-  type: PropTypes.string,
   icon: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired
+  type: PropTypes.string
 }
