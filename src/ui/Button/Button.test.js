22733d3b045f866a4c77370196977695bbd16798
Mats Andreassen <matsand@live.no>
2021-02-09 15:01:01 +0100

    La til tester på IconButton

--- src/ui/Button/Button.test.js
+++ src/ui/Button/Button.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 
-import { Button } from '.'
+import { Button, IconButton } from '.'
 
 describe('Button', () => {
   test('loads and displays button text', async () => {
@@ -67,6 +66,55 @@ describe('Button', () => {
   test('click on a button with a spinner shouldn\'t trigger onClick', async () => {
     const onClick = jest.fn()
     render(<Button onClick={onClick} spinner>Spinningknapp</Button>)
+    const btn = screen.getByRole('button')
+
+    fireEvent.click(btn)
+    expect(onClick).toBeCalledTimes(0)
+  })
+})
+
+describe('IconButton', () => {
+  test('loads and displays icon button text', async () => {
+    render(<IconButton icon='add'>Knappeteksten er her</IconButton>)
+    expect(screen.getByRole('button')).toHaveTextContent('Knappeteksten er her')
+  })
+
+  test('assigning class works and doesn\'t override default classes', async () => {
+    render(<IconButton icon='add' type='transparent-bordered' className='testclass'>Bordered</IconButton>)
+    const btn = screen.getByRole('button')
+
+    expect(btn).toHaveClass('icon-button')
+    expect(btn).toHaveClass('icon-button-transparent-bordered')
+    expect(btn).toHaveClass('testclass')
+  })
+
+  test('verify that correct icon is added', async () => {
+    render(<IconButton icon='close'>Close icon</IconButton>)
+    const svg = document.getElementsByTagName('svg')[0]
+    expect(svg).toHaveAttribute('data-jest-svg-name', 'icon-close')
+  })
+
+  test('one click on the button should trigger onClick once', async () => {
+    const onClick = jest.fn()
+    render(<IconButton icon='add' onClick={onClick}>Klikkikonknapp</IconButton>)
+    const btn = screen.getByRole('button')
+
+    fireEvent.click(btn)
+    expect(onClick).toBeCalledTimes(1)
+  })
+
+  test('click on a disabled button shouldn\'t trigger onClick', async () => {
+    const onClick = jest.fn()
+    render(<IconButton icon='add' onClick={onClick} disabled>Disablaikonknapp</IconButton>)
+    const btn = screen.getByRole('button')
+
+    fireEvent.click(btn)
+    expect(onClick).toBeCalledTimes(0)
+  })
+
+  test('click on a button with a spinner shouldn\'t trigger onClick', async () => {
+    const onClick = jest.fn()
+    render(<IconButton icon='add' onClick={onClick} spinner>Spinningikonknapp</IconButton>)
     const btn = screen.getByRole('button')
 
     fireEvent.click(btn)
