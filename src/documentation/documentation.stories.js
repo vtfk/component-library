3fed598955e077ddec9e38947eee6b17c2c87225
Mats Andreassen <matsand@live.no>
2021-02-11 20:34:13 +0100

    Oppdatert dokumentasjonssiden til å highlighe kode med hightlight.js

--- src/documentation/documentation.stories.js
+++ src/documentation/documentation.stories.js
@@ -1,12 +1,11 @@
-import marked from 'marked'
 import React from 'react'
+import marked from 'marked'
+import hljs from 'highlight.js'
+
 import packageJson from '../../package.json'
-
 import readMe from '../../README.md'
-// import developerReadMe from '../../scripts/build/README_PACKAGE.md'
 
-import changeLog from '../../CHANGELOG.md'
-
+import 'highlight.js/styles/atom-one-dark.css'
 import './documentation.css'
 
 export default {
@@ -18,17 +17,26 @@ export default {
     layout: 'centered'
   }
 }
-}
 
 function versionizeReadMe (readMeContent) {
   const readMeLines = readMeContent.split('\n')
-  return [readMeLines[0] + `<div class="version">Versjon ${packageJson.version}<div>`, ...readMeLines.slice(1)].join('\n')
+  return [
+    '<img src="https://designmanual.vtfk.no/css/images/VT-logo.svg" height="75px" class="logo" alt="" /><br />',
+    readMeLines[0] + `<div class="version"> v${packageJson.version}<div>`,
+    ...readMeLines.slice(1)
+  ].join('\n')
+}
+
+function convertMarkdown (markdown) {
+  marked.setOptions({
+    highlight: function (code, lang) {
+      return hljs.highlight(lang, code).value
+    }
+  })
+
+  return marked(markdown)
 }
 
 export function Introduksjon () {
-  return <div className='documentation' dangerouslySetInnerHTML={{ __html: marked(versionizeReadMe(readMe)) }} />
-}
-
-export function Changelog () {
-  return <div className='documentation' dangerouslySetInnerHTML={{ __html: marked(changeLog) }} />
+  return <div className='documentation' dangerouslySetInnerHTML={{ __html: convertMarkdown(versionizeReadMe(readMe)) }} />
 }
