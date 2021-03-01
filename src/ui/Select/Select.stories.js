359b36df6ed22d97aef84c2acfd1b5557b97d2c0
Mats Andreassen <matsand@live.no>
2021-03-01 14:44:30 +0100

    0.2.1

--- src/ui/Select/Select.stories.js
+++ src/ui/Select/Select.stories.js
@@ -20,6 +20,7 @@ export function Basic () {
   ]
 
   return (
+    <>
     <Select
       placeholder={text('Placeholder', 'Velg en årsak for varselet')}
       label={text('Label', 'Valgt årsak for varselet')}
@@ -30,6 +31,28 @@ export function Basic () {
       onChange={(item) => { setSelectedItem(item) }}
       style={{ margin: '20px' }}
     />
+        <Select
+      placeholder={text('Placeholder', 'Velg en årsak for varselet')}
+      label={text('Label', 'Valgt årsak for varselet')}
+      isOpen={boolean('Initially open', false)}
+      closeOnSelect={boolean('Close on selection', true)}
+      items={object('Items', items)}
+      selectedItem={selectedItem}
+      onChange={(item) => { setSelectedItem(item) }}
+      style={{ margin: '20px' }}
+    />
+    <Select
+      placeholder={text('Placeholder', 'Velg en årsak for varselet')}
+      label={text('Label', 'Valgt årsak for varselet')}
+      isOpen={boolean('Initially open', false)}
+      closeOnSelect={boolean('Close on selection', true)}
+      items={object('Items', items)}
+      selectedItem={selectedItem}
+      onChange={(item) => { setSelectedItem(item) }}
+      style={{ margin: '20px' }}
+    />
+
+    </>
   )
 }
 
