0ea222574af98e603987241381e934874e09a0be
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-01-19 21:08:14 +0100

    Must actually use checked prop in story aswell

--- src/ui/Checkbox/Checkbox.stories.js
+++ src/ui/Checkbox/Checkbox.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
 
 import { Checkbox } from '.'
@@ -8,7 +8,14 @@ export default getConfig(
 )
 
 export function Basic () {
+  const [checked, setChecked] = useState(false)
+
+  function onChange () {
+    setChecked(!checked)
+    console.log('onChange!')
+  }
+
   return (
-    <Checkbox name='gruppe-navn' value='verdi' label='Dette er labelen' onChange={() => { console.log('onChange!') }} />
+    <Checkbox name='gruppe-navn' value='verdi' label='Dette er labelen' checked={checked} onChange={() => { onChange() }} />
   )
 }
