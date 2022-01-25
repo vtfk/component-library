2ae4bb88235c22f864d8814a9e9ebd007c98a4ab
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2022-01-25 10:30:59 +0100

    disable checkbox

--- src/ui/Checkbox/index.js
+++ src/ui/Checkbox/index.js
@@ -5,12 +5,12 @@ import { ReactComponent as IconCheck } from './icon-check.svg'
 
 import './styles.scss'
 
-export function Checkbox ({ name, value, label, checked, onChange, ...props }) {
+export function Checkbox ({ name, value, label, checked, disabled, onChange, ...props }) {
   return (
     <div className='checkbox' {...props}>
       <div className='checkbox-inner'>
         <div className='check-wrapper'>
-          <input id={`check-${name}-${value}`} type='checkbox' name={name} value={value} checked={checked} onChange={onChange} {...props} tabIndex='0' />
+          <input id={`check-${name}-${value}`} type='checkbox' name={name} value={value} checked={checked} disabled={disabled || false} onChange={onChange} {...props} tabIndex='0' />
           <div className='check'>
             <IconCheck />
           </div>
@@ -23,6 +23,7 @@ export function Checkbox ({ name, value, label, checked, onChange, ...props }) {
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
+  disabled: PropTypes.bool,
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func,
