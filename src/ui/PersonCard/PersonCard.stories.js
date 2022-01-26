e86e7198c34d7c845b83f2065e80388fbd32ab0f
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2022-01-26 09:03:29 +0100

    Add PersonCard component

---
+++ src/ui/PersonCard/PersonCard.stories.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { getConfig } from '../../../scripts/storybook/storyConfig'
+
+import { PersonCard } from '.'
+
+import { Paragraph, Link } from '../Typography'
+import { InitialsBadge } from '../InitialsBadge'
+import { Skeleton } from '../Skeleton'
+
+export default getConfig(
+  { title: 'PersonCard', component: PersonCard }
+)
+
+export function Basic () {
+  return (
+    <PersonCard firstName='Per' lastName='Hansen'>
+      { /* Pass relevant content  */ }
+      <Paragraph>
+        Skolenavn
+      </Paragraph>
+      <Paragraph>
+        <Link href={'https://link.com'}>Klassekode</Link>
+      </Paragraph>
+      <Paragraph>
+        epost@epost.com
+      </Paragraph>
+      { /* END: Pass relevant content  */ }
+    </PersonCard>
+  )
+}
+
+export function Loading () {
+  const loading = true
+
+  return (
+    <PersonCard loading={true} firstName='Per' lastName='Hansen'>
+      { /* Pass relevant content  */ }
+      <Paragraph>
+        {loading ? <Skeleton width='200px' /> : 'Skolenavn'}
+      </Paragraph>
+      <Paragraph>
+        {loading ? <Skeleton width='180px' /> : <Link href={'https://link.com'}>Klassekode</Link>}
+      </Paragraph>
+      <Paragraph>
+        {loading ? <Skeleton width='180px' /> : 'epost@epost.com'}
+      </Paragraph>
+      { /* END: Pass relevant content  */ }
+    </PersonCard>
+  )
+}
---
+++ src/ui/PersonCard/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Heading2, Heading3, Paragraph, Link } from '../Typography'
+import { InitialsBadge } from '../InitialsBadge'
+import { Skeleton } from '../Skeleton'
+
+import './styles.scss'
+
+export function PersonCard ({ loading, firstName, lastName, largeName, ...props }) {
+  const NameHeading = largeName ? Heading2 : Heading3
+
+  return (
+    <div className='person-card'>
+      <div className='image'>
+        {
+          loading
+            ? <Skeleton variant='circle'><InitialsBadge size='large' /></Skeleton>
+            : <InitialsBadge firstName={firstName} lastName={lastName} size='large' />
+        }
+      </div>
+      <div className='text-wrapper'>
+        <NameHeading className='name'>
+          {
+            loading
+              ? <Skeleton randomWidth={[50, 100]} />
+              : `${firstName || ''} ${lastName}`
+          }
+        </NameHeading>
+        <div className='other'>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+PersonCard.propTypes = {
+  className: PropTypes.string,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  size: PropTypes.string
+}
