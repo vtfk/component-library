6c85d2fcea93d171b7ead477e1042df08af6cc56
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2022-02-08 22:19:13 +0100

    Cleanup

--- src/ui/SearchResult/SearchResult.stories.js
+++ src/ui/SearchResult/SearchResult.stories.js
@@ -65,24 +65,3 @@ export function Empty () {
     />
   )
 }
-
-// export function Sizes () {
-//   return (
-//     <table>
-//       <tbody>
-//         <tr>
-//           <td>small</td>
-//           <td><InitialsBadge size='small' firstName='Per' lastName='Hansen' /></td>
-//         </tr>
-//         <tr>
-//           <td>default</td>
-//           <td><InitialsBadge firstName='Fred' lastName='Aker' /></td>
-//         </tr>
-//         <tr>
-//           <td>large</td>
-//           <td><InitialsBadge size='large' firstName='Nils' lastName='Pedersen' /></td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   )
-// }
