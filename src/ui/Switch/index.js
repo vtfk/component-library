303f07a71703d244d47d1c1dad6f7000fbb41897
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-02-14 16:24:19 +0100

    Fjernet unødvendig div

--- src/ui/Switch/index.js
+++ src/ui/Switch/index.js
@@ -8,21 +8,19 @@ export function Switch ({ isActive, label, onClick, disabled, ...props }) {
   const id = nanoid();
   return (
       <div className="switch">
-        <div className="switch-inner">
-          <div className="switch-wrapper">
-          <button
-            id={`switch-${id}`}
-            className={`switch-element ${isActive ? 'active' : ''}`}
-            aria-label='Bryter'
-            aria-checked={isActive ? 'true' : 'false'}
-            role='switch'
-            disabled={disabled || false}
-            onClick={onClick}
-          />
-          {
-            label && <label className={disabled ? 'disabled' : ''} htmlFor={`switch-${id}`}>{label}</label>
-          }
-          </div>
+        <div className="switch-wrapper">
+        <button
+          id={`switch-${id}`}
+          className={`switch-element ${isActive ? 'active' : ''}`}
+          aria-label='Bryter'
+          aria-checked={isActive ? 'true' : 'false'}
+          role='switch'
+          disabled={disabled || false}
+          onClick={onClick}
+        />
+        {
+          label && <label className={disabled ? 'disabled' : ''} htmlFor={`switch-${id}`}>{label}</label>
+        }
         </div>
     </div>
   )
