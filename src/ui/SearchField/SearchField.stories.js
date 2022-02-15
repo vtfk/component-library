83158981c888418c6c1f21bbc24a982b9b965321
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-02-15 21:09:56 +0100

    Show how debounce works

--- src/ui/SearchField/SearchField.stories.js
+++ src/ui/SearchField/SearchField.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withKnobs, number } from '@storybook/addon-knobs'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
 
@@ -15,23 +15,51 @@ export function Basic () {
     <SearchField
       placeholder='Dette er placeholderen'
       value=''
-      onChange={event => console.log('onChange kjøres ved hver endring:', event.target.value)}
-      onSearch={() => console.log('onSearch!')}
+      onChange={e => console.log('onChange kjøres ved hver endring:', e.target.value)}
+      onSearch={() => console.log('onSearch kjøres ved "Enter" eller klikk på søkeknappen')}
       rounded
     />
   )
 }
 
 export function Debounce () {
+  const [value, setValue] = useState('')
+  const [debounced, setDebounced] = useState('')
+
+  function onChange (e) {
+    console.log('onChange kjører ved hver endring:', e.target.value)
+    setValue(e.target.value)
+  }
+
+  function onDebounce (e) {
+    console.log('onDebounce kjøres først når delay er ferdig:', e.target.value)
+    setDebounced(e.target.value)
+  }
+
   return (
-    <SearchField
-      debounceMs={number('Delay i millisekunder', 1000)}
-      onChange={event => console.log('onChange kjører ved hver endring:', event.target.value)}
-      onDebounce={event => console.log('onDebounce kjøres først når delay er ferdig:', event.target.value)}
-      placeholder='Søk utføres først etter 1 sekund'
-      value=''
-      onSearch={() => console.log('onSearch!')}
-      rounded
-    />
+    <div>
+      <SearchField
+        debounceMs={number('Delay i millisekunder', 1000)}
+        onChange={e => onChange(e)}
+        onDebounce={e => onDebounce(e)}
+        placeholder='Søk utføres først etter 1 sekund'
+        value=''
+        onSearch={() => console.log('onSearch kjøres ved "Enter" eller klikk på søkeknappen')}
+        rounded
+      />
+      <br />
+      <table>
+        <tbody>
+          <tr>
+            <td><strong>onChange</strong></td>
+            <td>{value}</td>
+          </tr>
+          <tr>
+            <td><strong>onDebounce</strong></td>
+            <td>{debounced}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
