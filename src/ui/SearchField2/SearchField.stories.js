2d170839f10600469d0d5c1a6b762b3d12a8d7a9
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-02-19 17:05:02 +0100

    Added SearchField with results

---
+++ src/ui/SearchField2/SearchField.stories.js
@@ -0,0 +1,165 @@
+import React, { useState } from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { withKnobs, number, text } from '@storybook/addon-knobs'
+import { getConfig } from '../../../scripts/storybook/storyConfig'
+
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
+import { SearchField } from '.'
+
+export default getConfig({
+  title: 'SearchField2',
+  component: SearchField,
+  decorators: [withKnobs]
+})
+
+const defaultItems = [
+  {
+    itemTitle: 'Sam Sam',
+    itemSecondary: 'sam0101',
+    itemDescription: 'Some place'
+  },
+  {
+    itemTitle: 'Tam Tam',
+    itemSecondary: 'tam0202',
+    itemDescription: 'Tam place'
+  },
+  {
+    itemTitle: 'Ram Ram',
+    itemSecondary: 'ram0303',
+    itemDescription: 'Rome place'
+  },
+  {
+    itemTitle: 'Bam Bam',
+    itemSecondary: 'bam0404',
+    itemDescription: 'Bome place'
+  },
+  {
+    itemTitle: 'Lam Lam',
+    itemSecondary: 'lam0505',
+    itemDescription: 'Lome place'
+  }
+]
+
+export function Basic () {
+  return (
+    <SearchField
+      placeholder='Dette er placeholderen'
+      value=''
+      onChange={e => console.log('onChange kjøres ved hver endring:', e.target.value)}
+      onSearch={value => console.log('onSearch kjøres ved "Enter" eller klikk på søkeknappen', value)}
+      rounded
+    />
+  )
+}
+
+export function Debounce () {
+  const [value, setValue] = useState('')
+  const [debounced, setDebounced] = useState('')
+
+  function onChange (e) {
+    console.log('onChange kjører ved hver endring:', e.target.value)
+    setValue(e.target.value)
+  }
+
+  function onDebounce (e) {
+    console.log('onDebounce kjøres først når delay er ferdig:', e.target.value)
+    setDebounced(e.target.value)
+  }
+
+  return (
+    <div>
+      <SearchField
+        debounceMs={number('Delay i millisekunder', 1000)}
+        onChange={e => onChange(e)}
+        onDebounce={e => onDebounce(e)}
+        placeholder='Søk utføres først etter 1 sekund'
+        value=''
+        onSearch={() => console.log('onSearch kjøres ved "Enter" eller klikk på søkeknappen')}
+        rounded
+      />
+      <br />
+      <table>
+        <tbody>
+          <tr>
+            <td><strong>onChange</strong></td>
+            <td>{value}</td>
+          </tr>
+          <tr>
+            <td><strong>onDebounce</strong></td>
+            <td>{debounced}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export function DebounceSearch () {
+  const [value, setValue] = useState('')
+  const [debounced, setDebounced] = useState('')
+  const [searched, setSearched] = useState('')
+  const [searching, setSearching] = useState(false)
+  const [items, setItems] = useState(defaultItems)
+
+  function onChange (e) {
+    console.log('onChange kjører ved hver endring:', e.target.value)
+    setValue(e.target.value)
+    setSearching(true)
+  }
+
+  function onDebounce (e) {
+    console.log('onDebounce kjøres først når delay er ferdig:', e.target.value)
+    setDebounced(e.target.value)
+    setItems(defaultItems.filter(item => item.itemTitle.toLowerCase().includes(e.target.value) || item.itemSecondary.toLowerCase().includes(e.target.value) || item.itemDescription.toLowerCase().includes(e.target.value)))
+    setSearching(false)
+  }
+
+  function onSearch (value) {
+    console.log('onSearch kjøres ved "Enter", klikk på søkeknappen eller når en oppføring i lista velges', value)
+    setSearched(JSON.stringify(value, null, 2))
+  }
+
+  return (
+    <div>
+      <SearchField
+        debounceMs={number('Delay i millisekunder', 1000)}
+        onChange={e => onChange(e)}
+        onDebounce={e => onDebounce(e)}
+        placeholder='Søk utføres først etter 1 sekund'
+        value=''
+        onSearch={e => onSearch(e)}
+        rounded
+        loading={searching}
+        loadingText={text('Loading text', 'Søker... (her kan man sette inn tekst eller HTML)')}
+        emptyText={text('Empty text', 'Søket gav ingen resultater... (her kan man sette inn tekst eller HTML)')}
+        items={items}
+        onItemClick={e => onSearch(e)}
+      />
+      <br />
+      <table>
+        <tbody>
+          <tr>
+            <td><strong>onChange</strong></td>
+            <td>{value}</td>
+          </tr>
+          <tr>
+            <td><strong>onDebounce</strong></td>
+            <td>{debounced}</td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: 'text-bottom', paddingTop: `${searched ? '25px' : '1px'}` }}><strong>onSearch</strong></td>
+            <td><SyntaxHighlighter language='json' style={docco} wrapLines customStyle={{ background: 'none' }}>{searched}</SyntaxHighlighter></td>
+          </tr>
+        </tbody>
+      </table>
+      <br /><br />
+      <div>
+        Items i lista:
+        <SyntaxHighlighter language='json' style={docco} wrapLines customStyle={{ background: 'none' }}>
+          {JSON.stringify(defaultItems, null, 2)}
+        </SyntaxHighlighter>
+      </div>
+    </div>
+  )
+}
---
+++ src/ui/SearchField2/index.js
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { useDebouncedCallback } from 'use-debounce'
+
+import './styles.scss'
+
+import { TextField } from '../TextField'
+import { Icon } from '../Icon'
+import { Paragraph } from '../Typography'
+
+export function SearchField ({ placeholder, value, debounceMs, onDebounce, rounded, onSearch, onChange, className, items, loading, loadingText, emptyText, onItemClick, ...props }) {
+  const [searchValue, setSearchValue] = useState(value || '')
+  const [searchInputFocused, setSearchInputFocused] = useState(false)
+  const [searchInputSelectedIndex, setSearchInputSelectedIndex] = useState(0)
+  const debouncer = useDebouncedCallback(event => {
+    if (onDebounce && typeof onDebounce === 'function') onDebounce(event)
+  }, debounceMs)
+
+  useEffect(() => {
+    if (typeof loading === 'boolean' && loading) setSearchInputSelectedIndex(0)
+  }, [loading])
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'ArrowUp') {
+      event.preventDefault()
+      if (items && items.length > 0 && searchInputSelectedIndex > 0) setSearchInputSelectedIndex(searchInputSelectedIndex - 1)
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault()
+      if (items && (items.length - 1) > searchInputSelectedIndex) setSearchInputSelectedIndex(searchInputSelectedIndex + 1)
+    } else if (event.key === 'Enter') {
+      event.preventDefault()
+
+      search()
+    }
+  }
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value)
+    debouncer(event)
+    if (onChange && typeof onChange === 'function') onChange(event)
+  }
+
+  const handleItemClick = (item, index) => {
+    if (onItemClick && typeof onItemClick === 'function') onItemClick(item)
+    setSearchInputSelectedIndex(index)
+  }
+
+  const search = () => {
+    if (onSearch && typeof onSearch === 'function') onSearch(items && items.length > 0 ? items[searchInputSelectedIndex] : searchValue)
+  }
+
+  return (
+    <div className='header-search'>
+      <div className={`search-field ${rounded ? 'rounded' : ''}`}>
+        <TextField
+          value={searchValue}
+          className={`${className || ''}`}
+          rounded={rounded}
+          placeholder={placeholder || 'Søk...'}
+          label={rounded ? null : placeholder}
+          onBlur={() => { setSearchInputFocused(false) }}
+          onFocus={() => { setSearchInputFocused(true) }}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          style={
+            items && searchInputFocused && searchValue !== ''
+              ? { boxShadow: 'none', paddingRight: 200, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderColor: '#979797', borderBottomWidth: 0 }
+              : { boxShadow: 'none', paddingRight: 200, borderColor: '#979797' }
+          }
+          {...props}
+        />
+
+        <div className='icon' onClick={search}>
+          <Icon name='search' alt='' />
+        </div>
+      </div>
+      {
+        items && searchInputFocused && searchValue !== '' &&
+          <div className='search-result'>
+            <div className='search-results-inner'>
+              {
+                !loading &&
+                items &&
+                items.length > 0 &&
+                items.map((item, index) => {
+                  return (
+                    <div onMouseDown={() => { handleItemClick(item, index) }} key={index} className={`search-results-item ${index === searchInputSelectedIndex ? 'active' : ''}`}>
+                      <Paragraph className='search-results-item-title'>{item.itemTitle}</Paragraph>
+                      {
+                        item.itemSecondary &&
+                          <Paragraph className='search-results-item-secondary' size='small'>{item.itemSecondary}</Paragraph>
+                      }
+                      {
+                        item.itemDescription &&
+                          <Paragraph className='search-results-item-description' size='small'>{item.itemDescription}</Paragraph>
+                      }
+                    </div>
+                  )
+                })
+              }
+
+              {
+                !loading &&
+                items &&
+                items.length === 0 &&
+                  <div className='search-results-item-message search-alternatives'>
+                    <Paragraph>
+                      {emptyText}
+                    </Paragraph>
+                  </div>
+              }
+
+              {
+                loading &&
+                  <div className='search-results-item-message search-alternatives'>
+                    <Paragraph>
+                      {loadingText}
+                    </Paragraph>
+                  </div>
+              }
+            </div>
+          </div>
+      }
+    </div>
+  )
+}
+
+SearchField.propTypes = {
+  className: PropTypes.string,
+  debounceMs: PropTypes.number,
+  emptyText: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+  items: PropTypes.arrayOf(PropTypes.shape({
+    itemTitle: PropTypes.string.isRequired,
+    itemSecondary: PropTypes.string.isRequired,
+    itemDescription: PropTypes.string.isRequired
+  })),
+  loading: PropTypes.bool,
+  loadingText: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+  onChange: PropTypes.func,
+  onDebounce: PropTypes.func,
+  onItemClick: PropTypes.func,
+  onSearch: PropTypes.func,
+  placeholder: PropTypes.string,
+  rounded: PropTypes.bool,
+  value: PropTypes.string
+}
+
+SearchField.defaultProps = {
+  debounceMs: 0,
+  emptyText: 'Søket gav ingen resultater...',
+  loadingText: 'Søker...'
+}
