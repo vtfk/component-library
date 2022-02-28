4bc5791b8c306ef4a43716ca26d8c390e24bb14e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-02-28 14:10:59 +0100

    Use size prop for StatisticsCard Aswell as removed the table since it wasn't needed

--- src/ui/Statistics/Statistics.stories.js
+++ src/ui/Statistics/Statistics.stories.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
+import { withKnobs, select } from '@storybook/addon-knobs'
 
 import { StatisticsCard, StatisticsGroup, StatisticsProgressBar } from '.'
 
 const apples = 15
 
+const cardSize = [
+  'large',
+  'small'
+]
+
 export default getConfig({
   title: 'Statistics',
   component: StatisticsCard,
+  decorators: [withKnobs],
   subcomponents: {
     StatisticsGroup,
     StatisticsProgressBar
@@ -17,43 +24,31 @@ export default getConfig({
 export function Basic () {
   return (
     <div>
-      <table>
-        <tbody>
-          <tr>
-            <td>One single <b>StatisticsCard</b> in a <b>StatisticsGroup</b></td>
-          </tr>
-          <tr>
-            <td>
-              <StatisticsGroup type='card'>
-                <StatisticsCard title='Is how many apples i have'>
-                  {
-                    apples
-                  }
-                </StatisticsCard>
-              </StatisticsGroup>
-            </td>
-          </tr>
-          <tr>
-            <td>Two <b>StatisticsCard</b> in a <b>StatisticsGroup</b></td>
-          </tr>
-          <tr>
-            <td>
-              <StatisticsGroup type='card'>
-                <StatisticsCard title='Is half of my apples'>
-                  {
-                    apples / 2
-                  }
-                </StatisticsCard>
-                <StatisticsCard title='Is a third of my apples'>
-                  {
-                    apples / 3
-                  }
-                </StatisticsCard>
-              </StatisticsGroup>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        One single <b>StatisticsCard</b> in a <b>StatisticsGroup</b><br />
+        <StatisticsGroup type='card'>
+          <StatisticsCard title='Is how many apples i have' size={select('Size', cardSize, 'small')}>
+            {
+              apples
+            }
+          </StatisticsCard>
+        </StatisticsGroup>
+      </div>
+      <div>
+        Two <b>StatisticsCard</b> in a <b>StatisticsGroup</b><br />
+        <StatisticsGroup type='card'>
+          <StatisticsCard title='Is half of my apples' size={select('Size', cardSize, 'small')}>
+            {
+              apples / 2
+            }
+          </StatisticsCard>
+          <StatisticsCard title='Is a third of my apples' size={select('Size', cardSize, 'small')}>
+            {
+              apples / 3
+            }
+          </StatisticsCard>
+        </StatisticsGroup>
+      </div>
     </div>
   )
 }
@@ -61,23 +56,13 @@ export function Basic () {
 export function Progress () {
   return (
     <div>
-      <table>
-        <tbody>
-          <tr>
-            <td><h2>Four <b>StatisticsProgress</b> in a <b>StatisticsGroup</b></h2></td>
-          </tr>
-          <tr>
-            <td>
-              <StatisticsGroup type='progress'>
-                <StatisticsProgressBar name='How many apples do i have?' value={100} maxValue={100} />
-                <StatisticsProgressBar name='How many apples is half?' value={50} maxValue={100} />
-                <StatisticsProgressBar name='How many apples is a third?' value={33} maxValue={100} />
-                <StatisticsProgressBar name='How many apples is 1?' value={1} maxValue={100} />
-              </StatisticsGroup>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <h2>Four <b>StatisticsProgress</b> in a <b>StatisticsGroup</b></h2>
+      <StatisticsGroup type='progress'>
+        <StatisticsProgressBar name='How many apples do i have?' value={100} maxValue={100} />
+        <StatisticsProgressBar name='How many apples is half?' value={50} maxValue={100} />
+        <StatisticsProgressBar name='How many apples is a third?' value={33} maxValue={100} />
+        <StatisticsProgressBar name='How many apples is 1?' value={1} maxValue={100} />
+      </StatisticsGroup>
     </div>
   )
 }
