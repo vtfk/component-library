f5fc5f384133ed6a274b9e577ebae73120a384bf
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-03 15:27:59 +0100

    Autohide `SideNav` when screen width <= 1000px

--- src/ui/SideNav/SideNav.stories.js
+++ src/ui/SideNav/SideNav.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
 
 import { SideNav, SideNavItem } from '.'
@@ -9,11 +9,29 @@ export default getConfig(
 )
 
 export function Basic () {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize (event) {
+      setWidth(event.target.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
   return (
-    <SideNav title='SideNav'>
-      <SideNavItem href='https://vtfk.no' icon={<Icon name='home' size='small' />} title='Link 1' active />
-      <SideNavItem href='https://vtfk.no' icon={<Icon name='students' size='small' />} title='Link 2' />
-      <SideNavItem href='https://vtfk.no' icon={<Icon name='classes' size='small' />} title='Link 3' />
-    </SideNav>
+    <>
+      <SideNav title='SideNav'>
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='home' size='small' />} title='Link 1' active />
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='students' size='small' />} title='Link 2' />
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='classes' size='small' />} title='Link 3' />
+      </SideNav>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <span style={{ paddingBottom: '1em' }}><strong>SideNav</strong> vil være synlig når <i>innerWidth</i> er større enn <strong>1000px</strong>.</span>
+        <span style={{ paddingBottom: '1em' }}>Går <i>innerWidth</i> til <strong>1000px</strong> eller mindre skjules den automatisk</span>
+        <span><strong>innerWidth</strong>: {width}px</span><br /><br />
+      </div>
+    </>
   )
 }
