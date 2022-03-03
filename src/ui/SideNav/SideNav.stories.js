a7c79cde4dd707f1768fb50cece1ba68dc154c7d
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-03 22:12:52 +0100

    Moved `TopNav` into SideNav as `SideNavMini`

--- src/ui/SideNav/SideNav.stories.js
+++ src/ui/SideNav/SideNav.stories.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { getConfig } from '../../../scripts/storybook/storyConfig'
 
-import { SideNav, SideNavItem } from '.'
+import { SideNav, SideNavItem, SideNavMini, SideNavMiniItem } from '.'
 import { Icon } from '../Icon'
 
 export default getConfig({
   title: 'SideNav',
   component: SideNav,
   subcomponents: {
-    SideNavItem
+    SideNavItem,
+    SideNavMini,
+    SideNavMiniItem
   }
 })
 
@@ -35,6 +37,71 @@ export function Basic () {
         <span style={{ paddingBottom: '1em' }}><strong>SideNav</strong> vil være synlig når <i>innerWidth</i> er større enn <strong>1000px</strong>.</span>
         <span style={{ paddingBottom: '1em' }}>Går <i>innerWidth</i> til <strong>1000px</strong> eller mindre skjules den automatisk</span>
         <span><strong>innerWidth</strong>: {width}px</span><br /><br />
+      </div>
+    </>
+  )
+}
+
+export function Mini () {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize (event) {
+      setWidth(event.target.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  return (
+    <>
+      <SideNavMini title='SideNavMini'>
+        <SideNavMiniItem title='Link 1' onClick={() => console.log('Link 1 clicked')} />
+        <SideNavMiniItem title='Link 2' onClick={() => console.log('Link 2 clicked')} />
+        <SideNavMiniItem href='/' title='Link 3' />
+      </SideNavMini>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <span style={{ paddingBottom: '1em' }}><strong>SideNavMini</strong> vil være synlig når <i>innerWidth</i> er mindre enn eller lik <strong>1000px</strong>.</span>
+        <span style={{ paddingBottom: '1em' }}>Går <i>innerWidth</i> til <strong>1001px</strong> eller over skjules den automatisk</span>
+        <span><strong>innerWidth</strong>: {width}px</span><br /><br />
+      </div>
+    </>
+  )
+}
+
+export function Both () {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize (event) {
+      setWidth(event.target.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  return (
+    <>
+      <SideNav title='SideNav'>
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='home' size='small' />} title='Link 1' active />
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='students' size='small' />} title='Link 2' />
+        <SideNavItem href='https://vtfk.no' icon={<Icon name='classes' size='small' />} title='Link 3' />
+      </SideNav>
+      <SideNavMini title='SideNavMini'>
+        <SideNavMiniItem title='Link 1' onClick={() => console.log('Link 1 clicked')} />
+        <SideNavMiniItem title='Link 2' onClick={() => console.log('Link 2 clicked')} />
+        <SideNavMiniItem href='/' title='Link 3' />
+      </SideNavMini>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <span style={{ paddingBottom: '1em' }}><strong>SideNav</strong> vil være synlig når <i>innerWidth</i> er større enn <strong>1000px</strong>.</span>
+        <span style={{ paddingBottom: '1em' }}>Går <i>innerWidth</i> til <strong>1000px</strong> eller mindre skjules den automatisk.</span><br /><br />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <span style={{ paddingBottom: '1em' }}><strong>SideNavMini</strong> vil være synlig når <i>innerWidth</i> er mindre enn eller lik <strong>1000px</strong>.</span>
+        <span style={{ paddingBottom: '1em' }}>Går <i>innerWidth</i> til <strong>1001px</strong> eller over skjules den automatisk.</span><br /><br />
+        <span><strong>innerWidth</strong>: {width}px</span>
       </div>
     </>
   )
--- src/ui/SideNav/index.js
+++ src/ui/SideNav/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { IconDropdownNav, IconDropdownNavItem } from '../IconDropdownNav'
 import { Logo } from '../Logo'
 
 import './styles.scss'
@@ -8,7 +9,8 @@ import './styles.scss'
 export function SideNav ({ title, ...props }) {
   return (
     <>
-      <nav className='sidenav' {...props}>
+      {/* Navigation shown for screen width > 1000px */}
+      <nav role='navigation' className='sidenav' {...props}>
         <div className='brand'>
           <div className='brand-logo'>
             <a href='/' aria-label='Hjem'>
@@ -29,6 +31,25 @@ export function SideNav ({ title, ...props }) {
   )
 }
 
+export function SideNavMini ({ title, ...props }) {
+  return (
+    <>
+      {/* Navigation shown for screen width <= 1000px */}
+      <nav role='navigation' className='sidenav-mini'>
+        <a href='/' className='brand'>
+          <div className='brand-logo' aria-hidden>
+            <Logo />
+          </div>
+          <div className='brand-name'>{title}</div>
+        </a>
+        <IconDropdownNav>
+          {props.children}
+        </IconDropdownNav>
+      </nav>
+    </>
+  )
+}
+
 export function SideNavItem ({ href, icon, title, active, ...props }) {
   return (
     <li className={`sidenav-item ${active === true ? 'active' : ''}`} {...props}>
@@ -42,7 +63,31 @@ export function SideNavItem ({ href, icon, title, active, ...props }) {
   )
 }
 
+export function SideNavMiniItem ({ href, title, onClick }) {
+  return (
+    <>
+      {
+        href &&
+          <IconDropdownNavItem closeOnClick href={href} title={title} />
+      }
+
+      {
+        onClick &&
+          <IconDropdownNavItem closeOnClick title={title} onClick={() => onClick()} />
+      }
+    </>
+  )
+}
+
 SideNav.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  title: PropTypes.string.isRequired
+}
+
+SideNavMini.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
@@ -54,5 +99,11 @@ SideNavItem.propTypes = {
   active: PropTypes.bool,
   href: PropTypes.string.isRequired,
   icon: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired
+}
+
+SideNavMiniItem.propTypes = {
+  href: PropTypes.string,
+  onClick: PropTypes.func,
   title: PropTypes.string.isRequired
 }
