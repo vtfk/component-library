fc6b0ebae5eb75d631b87f8a806c2de6e0c37fd1
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-28 15:26:16 +0200

    Lagt til mulighet for å laste ned PDF på nettlesere som ikke støtter PDF-visning

--- src/ui/PDFPreview/index.js
+++ src/ui/PDFPreview/index.js
@@ -4,24 +4,16 @@ import PropTypes from 'prop-types'
 import './styles.scss'
 
 export function PDFPreview ({ className, loading, base64, error }) {
-  let pdf = false
-
-  if (base64) {
-    pdf = React.createElement(
-      'object', {
-        type: 'application/pdf',
-        data: 'data:application/pdf;base64,' + base64,
-        width: '100%',
-        height: '100%',
-        'aria-label': 'PDF-dokument'
-      }
-    )
-  }
-
   return (
     <div className={className || 'preview-content'}>
       <div className={className ? className.includes('modal') ? 'preview-modal-body' : className : 'preview-body'}>
-        {base64 && pdf ? pdf : ''}
+        {
+          base64 &&
+            <object type='application/pdf' data={`data:application/pdf;base64,${base64}`} width='100%' height='100%' aria-label='PDF-dokument'>
+              <p>Din nettleser støtter ikke PDF-visning</p>
+              <p>Last ned filen <a href={`data:application/pdf;base64,${base64}`} download='preview.pdf'>her</a></p>
+            </object>
+        }
         {loading && !base64 && !error ? <div className={className ? className.includes('modal') ? 'preview-modal-loading' : className : 'preview-loading'}><span>.</span><span>.</span><span>.</span></div> : ''}
         {error ? <div className={className ? className.includes('modal') ? 'preview-modal-error' : className : 'preview-error'}>Det har skjedd en feil, vennligst prøv igjen.</div> : ''}
       </div>
